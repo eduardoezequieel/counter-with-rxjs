54002fd88fc91d9a6a4039cdcad08b3080a85e16
Eduardo Ezequiel <57075508+eduardoezequieel@users.noreply.github.com>
2022-08-11 12:02:42 -0600

    Changes

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-import { fromEvent } from "rxjs";
-import { map, tap } from "rxjs/operators";
+import { interval } from "rxjs";
+import { mapTo, scan, filter } from "rxjs/operators";
 
-function calculateScrollPercent(element) {
-  const { scrollTop, scrollHeight, clientHeight } = element;
+const countdown = document.getElementById("countdown");
+const message = document.getElementById("message");
+const counter$ = interval(1000);
 
-  return (scrollTop / (scrollHeight - clientHeight)) * 100;
-}
+counter$
+  .pipe(
+    mapTo(-1),
+    scan((accumulator, current) => {
+      return accumulator + current;
+    }, 10),
+    filter((value) => value >= 0)
+  )
+  .subscribe((value) => {
+    countdown.innerHTML = value;
 
-const progressBar = document.querySelector('.progress-bar')
-
-const scroll$ = fromEvent(document, "scroll");
-const progress$ = scroll$.pipe(
-  map(({ target }) => calculateScrollPercent(target.scrollingElement))
-);
-
-progress$.subscribe(percent=>{
-  progressBar.style.width = `${percent}%`
-});
+    if (!value) {
+      message.innerHTML = "Liftoff!";
+    }
+  });
